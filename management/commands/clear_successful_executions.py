eb1ccbe324addae13023163167343166de2ef9f8
Chris J. Karr <chris@audacious-software.com>
2021-07-19 23:26:41 +0000

    Made clear_successful_executions usable as a Task

--- management/commands/clear_successful_executions.py
+++ management/commands/clear_successful_executions.py
@@ -8,16 +8,19 @@ import datetime
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from ...decorators import handle_lock
+from ...decorators import handle_lock, handle_schedule, add_qs_arguments
 from ...models import Execution
+
 
 class Command(BaseCommand):
     help = 'Clears older Quicksilver successful task execution records.'
 
+    @add_qs_arguments
     def add_arguments(self, parser):
         parser.add_argument('--before_minutes', required=False, type=int, default=120, help='Removes successful task executions older than provided minutes.')
 
     @handle_lock
+    @handle_schedule
     def handle(self, *args, **options):
         before = timezone.now() - datetime.timedelta(seconds=(60 * options['before_minutes']))
 
