8ba4ed957c3ea647d5ca079ef5bdbae68010750d
Chris J. Karr <chris@audacious-software.com>
2021-10-01 02:08:22 +0000

    Explicitly setting DB ID type

--- apps.py
+++ apps.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-
 from django.apps import AppConfig
 
 
 class QuicksilverConfig(AppConfig):
     name = 'quicksilver'
+    default_auto_field = 'django.db.models.AutoField'
