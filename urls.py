afc66b0e135e2403d00151c732f021a51f5ece89
Chris Karr <chris@audacious-software.com>
2024-02-26 16:47:55 -0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,20 +1,20 @@
-# pylint: disable=line-too-long
+# pylint: disable=line-too-long, no-name-in-module
 
 import sys
 
 if sys.version_info[0] > 2:
     from django.urls import re_path
-    
+
     from .views import quicksilver_status
-    
+
     urlpatterns = [
         re_path(r'^status$', quicksilver_status, name='quicksilver_status'),
     ]
 else:
     from django.conf.urls import url
-    
+
     from .views import quicksilver_status
-    
+
     urlpatterns = [
         url(r'^status$', quicksilver_status, name='quicksilver_status'),
     ]
