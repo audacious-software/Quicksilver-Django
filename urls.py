a9c7317b32b6307a97ef8717969503d3c991f516
Chris J. Karr <chris@audacious-software.com>
2024-05-17 18:21:51 +0000

    Dependency updates

--- urls.py
+++ urls.py
@@ -3,18 +3,12 @@
 import sys
 
 if sys.version_info[0] > 2:
-    from django.urls import re_path
-
-    from .views import quicksilver_status
-
-    urlpatterns = [
-        re_path(r'^status$', quicksilver_status, name='quicksilver_status'),
-    ]
+    from django.urls import re_path as url # pylint: disable=no-name-in-module
 else:
     from django.conf.urls import url
 
-    from .views import quicksilver_status
+from .views import quicksilver_status
 
-    urlpatterns = [
-        url(r'^status$', quicksilver_status, name='quicksilver_status'),
-    ]
+urlpatterns = [
+    url(r'^status$', quicksilver_status, name='quicksilver_status'),
+]
